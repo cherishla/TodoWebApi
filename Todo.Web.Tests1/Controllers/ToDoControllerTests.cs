b147f4d65cb836c5a889cce3b9fac0daa7c06904
Will保哥 <doggy.huang@gmail.com>
2017-04-25 03:05:25 +0800

    加入 ToDoController 的 GetUnFinishItems() 測試方法

--- Todo.Web.Tests1/Controllers/ToDoControllerTests.cs
+++ Todo.Web.Tests1/Controllers/ToDoControllerTests.cs
@@ -33,5 +33,29 @@ namespace Todo.Web.Controllers.Tests
             // Assert
             result.Should().BeEquivalentTo(expected);
         }
+
+        [Test()]
+        public void GetUnFinishItemsTest()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var data1 = fixture.Build<TodoItem>()
+                .With(x => x.IsFinish, true)
+                .CreateMany(2);
+            var data2 = fixture.Build<TodoItem>()
+                .With(x => x.IsFinish, false)
+                .CreateMany(3);
+            var expected = data1.Concat(data2).ToList();
+
+            var todoService = Mock.Create<ITodoService>();
+            Mock.Arrange(() => todoService.GetAllItems()).ReturnsCollection(expected);
+
+            // Act
+            var controller = new ToDoController(todoService);
+            var result = controller.GetUnFinishItems();
+
+            // Assert
+            result.Should().BeEquivalentTo(data2);
+        }
     }
 }
