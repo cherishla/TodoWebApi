576da7b4d976d22cb36a52fdb77c95e11773ea67
Will保哥 <doggy.huang@gmail.com>
2017-04-25 02:57:49 +0800

    加入 ToDoController 的 GetAllItems() 測試方法
    建立 GetAllItems() 方法的測試程式
    NUnit 3
    
    在測試專案安裝 NuGet 套件
    Install-Package FluentAssertions
    Install-Package AutoFixture.NUnit3
    
    在測試專案加入 JustMock 組件
    "C:\Program Files (x86)\Telerik\JustMock\Libraries\Telerik.JustMock.dll"

---
+++ Todo.Web.Tests1/Controllers/ToDoControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Todo.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ploeh.AutoFixture;
+using Todo.DAL;
+using Telerik.JustMock;
+using Todo.Service;
+using FluentAssertions;
+
+namespace Todo.Web.Controllers.Tests
+{
+    [TestFixture()]
+    public class ToDoControllerTests
+    {
+        [Test()]
+        public void GetAllItemsTest()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var expected = fixture.CreateMany<TodoItem>(20);
+
+            var todoService = Mock.Create<ITodoService>();
+            Mock.Arrange(() => todoService.GetAllItems()).ReturnsCollection(expected);
+
+            // Act
+            var controller = new ToDoController(todoService);
+            var result = controller.GetAllItems();
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
